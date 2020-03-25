b0e2b1061887923ee3e4af19c2d216fe5d595355
Redshore <isutare412@gmail.com>
2020-03-25 23:42:04 +0900

    initial commit

---
+++ src/main/java/kr/ac/snu/ids/SimpleDBMS.java
@@ -0,0 +1,13 @@
+package kr.ac.snu.ids;
+
+/**
+ * Hello world!
+ *
+ */
+public class SimpleDBMS
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
